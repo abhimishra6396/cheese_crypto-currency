8f070b5afec9a4af78a2849dbb47fad68c33197c
abhimishra6396 <abhishek.mishra@etu.upmc.fr>
2020-03-26 16:30:24 +0100

    Update proof_of_work.py

--- proof_of_work.py
+++ proof_of_work.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
 
 	member_list = [] 
 
-	for i in range(5):
+	for i in range(2):
 		member_list.append(Member(i+1, randint(1000, 8999)))
 		member_list[i].activateMember()
 		member_list[i].startListening()
