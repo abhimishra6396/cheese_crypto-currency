f64d516279cdfb7106b85284c7e768ad834453bb
abhimishra6396 <abhishek.mishra@etu.upmc.fr>
2020-03-26 15:50:55 +0100

    Update proof_of_work.py

--- proof_of_work.py
+++ proof_of_work.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
 	member_list = [] 
 
 	for i in range(5):
-		member_list.append(Member(randint(1000, 8999)))
+		member_list.append(Member(i+1, randint(1000, 8999)))
 		member_list[i].activateMember()
 		member_list[i].startListening()
 	member_list[0].cheesestack.createCheese("D_E_1")
